afcacce87d222fd97f8457eebf1f1761a30bbe5e
Faiz <83203236+faiz@users.noreply.github.com>
2025-01-24 23:29:34 +0800

    Enhance FAQ component with motion effects for improved user engagement; update headings to use motion elements for better visibility.

--- components/AboutPage/FAQ.jsx
+++ components/AboutPage/FAQ.jsx
@@ -1,6 +1,7 @@
 'use client';
 import { faqLists } from '@/models/about';
 import React, { useState } from 'react';
+import { motion } from 'motion/react';
 
 const FAQ = () => {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -13,16 +14,29 @@ const FAQ = () => {
     <div className="section bg-zinc-900 text-white">
       <div className="flex flex-col md:flex-row w-full gap-4 pb-20">
         <div className="md:w-1/2 w-full flex flex-col px-6">
-          <p className="text-subtitle sticky top-16 z-10 text-zinc-300">FAQs</p>
-          <h3 className="text-title sticky top-24 py-4 z-10 text-2xl font-semibold text-zinc-100">
+          <motion.p
+          initial={{ y: 20, opacity: 0}}
+          whileInView={{ y: 0, opacity: 1}}
+          transition={{ duration: 0.5, delay: 0.3 }}
+           className="text-subtitle sticky top-16 z-10 text-zinc-300">FAQs</motion.p>
+          <motion.h3
+          initial={{ y: 20, opacity: 0}}
+          whileInView={{ y: 0, opacity: 1}}
+          transition={{ duration: 0.8, delay: 0.5 }}
+           className="text-title sticky top-24 py-4 z-10 text-2xl font-semibold text-zinc-100">
             Frequently Asked Questions
-          </h3>
+          </motion.h3>
         </div>
 
         <div className="md:w-1/2 w-full">
           <div className="space-y-4">
             {faqLists.map((faq, index) => (
-              <div key={index}>
+              <motion.div 
+              key={index}
+              initial={{ y: 20, opacity: 0}}
+              whileInView={{ y: 0, opacity: 1}}
+              transition={{ duration: 0.6 + (index/5), delay: 0.3 + (index/20) }}
+              >
                 <hr className="border-t-1 border-zinc-600 my-2" />
                 <div className="rounded-lg hover:bg-zinc-800">
                   <button
@@ -43,7 +57,7 @@ const FAQ = () => {
                     <p>{faq.answer}</p>
                   </div>
                 </div>
-              </div>
+              </motion.div>
             ))}
             <hr className="border-t-1 border-zinc-600 my-2" />
           </div>
