9d5a4c5e1e75877ea72316cb808bbe7f428abd12
Faiz Azmi <83203236+faizzmi@users.noreply.github.com>
2025-01-31 23:13:44 +0800

    Update HeaderImage.jsx

--- components/ProjectDetails/HeaderImage.jsx
+++ components/ProjectDetails/HeaderImage.jsx
@@ -3,24 +3,20 @@ import Image from 'next/image'
 import React from 'react'
 import { motion } from 'framer-motion'
 
-const HeaderImage = ({ img }: { img: string }) => {
-  if (!img) return null // Prevent rendering if no image is provided
-
+const HeaderImage = ({ img }) => {
   return (
     <motion.div
       initial={{ scale: 0.8 }}
       whileInView={{ scale: 1 }}
-      viewport={{ once: true }}
       transition={{ duration: 0.8, type: 'spring', stiffness: 100, bounce: 0 }}
-      className="relative w-[90%] md:w-[80%] lg:w-[70%] min-h-[50vh] md:h-screen mx-auto overflow-hidden"
+      className="relative w-full md:w-[80%] lg:w-[70%] h-[60vh] md:h-[70vh] lg:h-[80vh] mx-auto overflow-hidden"
     >
       <Image
         src={img}
         alt="Project image"
         fill
-        sizes="100vw"
-        className="object-contain"
-        priority
+        sizes="(max-width: 768px) 100vw, (max-width: 1024px) 80vw, 70vw"
+        className="object-cover"
       />
     </motion.div>
   )
